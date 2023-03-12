ef4b5ef9b6fa20920b95be6ecb01cc9e340d640c
jonathan <bayart.jonathan@laposte.net>
2023-03-12 20:36:22 +0100

    texte dynamique accueil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
+import SocialNetwork from '../components/SocialNetwork';
+import DynamicText from '../components/DynamicText';
 
 const Home = () => {
     return (
         <div>
+            <Mouse />
             <Navigation />
+            < SocialNetwork />
+            <div className='home-main'>
+                <div className='main-content'>
+                    <h1>JO AGENCY</h1>
+                    <DynamicText />
+                </div>
+            </div>
         </div>
     );
 };
