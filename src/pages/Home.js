967731e102d9910998e7b5ba351d996e6f5b7073
jonathan <bayart.jonathan@laposte.net>
2023-03-13 21:02:36 +0100

    animation word home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Mouse from '../components/Mouse';
-import Navigation from '../components/Navigation';
-import SocialNetwork from '../components/SocialNetwork';
-import DynamicText from '../components/DynamicText';
+import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
+import DynamicText from "../components/DynamicText";
 
 const Home = () => {
-    return (
-        <div>
-            <Mouse />
-            <Navigation />
-            < SocialNetwork />
-            <div className='home-main'>
-                <div className='main-content'>
-                    <h1>JO AGENCY</h1>
-                    <DynamicText />
-                </div>
-            </div>
+  return (
+    <div>
+      <Mouse />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>JO AGENCY</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
